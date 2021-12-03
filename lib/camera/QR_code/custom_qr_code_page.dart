ff0913852b61c4afa4f7272ad6a4699fc1953a33
马元 <38006525+Jamxscape@users.noreply.github.com>
2021-12-03 18:08:10 +0800

    更新二维码

--- lib/camera/QR_code/custom_qr_code_page.dart
+++ lib/camera/QR_code/custom_qr_code_page.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:elegant_flutter/camera/QR_code/custom_qr_preview_page.dart';
 import 'package:flutter/material.dart';
-import 'package:ai_barcode/ai_barcode.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class CustomQRCodePAge extends StatefulWidget {
   const CustomQRCodePAge({Key? key}) : super(key: key);
@@ -10,39 +12,18 @@ class CustomQRCodePAge extends StatefulWidget {
 }
 
 class _CustomQRCodePAgeState extends State<CustomQRCodePAge> {
-  late ScannerController _scannerController;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
 
   @override
   void initState() {
     super.initState();
-
-    _scannerController = ScannerController(scannerResult: (result) {
-      /// 返回最后的结果
-      Navigator.push(context, MaterialPageRoute(builder: (_) {
-        return CustomQRPreviewPage(
-          result: result,
-        );
-      }));
-    }, scannerViewCreated: () {
-      TargetPlatform platform = Theme.of(context).platform;
-      if (TargetPlatform.iOS == platform) {
-        Future.delayed(Duration(seconds: 2), () {
-          _scannerController.startCamera();
-          _scannerController.startCameraPreview();
-        });
-      } else {
-        _scannerController.startCamera();
-        _scannerController.startCameraPreview();
-      }
-    });
   }
 
   @override
   void dispose() {
     super.dispose();
-
-    _scannerController.stopCameraPreview();
-    _scannerController.stopCamera();
   }
 
   @override
@@ -52,13 +33,63 @@ class _CustomQRCodePAgeState extends State<CustomQRCodePAge> {
         title: Text('二维码标题'),
       ),
       body: Container(
-        color: Colors.black26,
-        width: 375,
-        height: 766,
-        child: PlatformAiBarcodeScannerWidget(
-          platformScannerController: _scannerController,
-        ),
-      ),
+          color: Colors.black26,
+          width: 375,
+          height: 766,
+          child: _buildQrView(context)),
     );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 300.0
+        : 800.0;
+    return Stack(
+      children: <Widget>[
+        QRView(
+          key: qrKey,
+          onQRViewCreated: _onQRViewCreated,
+          overlay: QrScannerOverlayShape(
+              borderColor: Colors.blue,
+              borderRadius: 10,
+              borderLength: 50,
+              borderWidth: 5,
+              cutOutSize: scanArea),
+          onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+        ),
+        Positioned(
+            left: 60,
+            top: 520,
+            child: Text('将条形码放入框内，即可安全扫描',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                )))
+      ],
+    );
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) async {
+      if (result != null) return;
+      result = scanData;
+      // log(result!.code.toString());
+      Navigator.push(context, MaterialPageRoute(builder: (_) {
+        return CustomQRPreviewPage(
+          result: result!.code.toString(),
+        );
+      }));
+    });
   }
 }
--- lib/camera/camera_page.dart
+++ lib/camera/camera_page.dart
@@ -1,12 +1,6 @@
 import 'package:elegant_flutter/camera/QR_code/custom_qr_code_page.dart';
 import 'package:elegant_flutter/camera/QR_code/qr_code_page.dart';
-import 'package:elegant_flutter/picker/date_picker/date_picker.dart';
-import 'package:elegant_flutter/picker/file_picker/file_picker.dart';
-import 'package:elegant_flutter/picker/image%20picker/image_picker_page.dart';
 import 'package:elegant_flutter/picker/image%20picker/wechat_assets_picker/wechat_take_photo_page.dart';
-import 'package:elegant_flutter/picker/multiple_picker/multiple_picker.dart';
-import 'package:elegant_flutter/picker/record/wechat_record_page.dart';
-import 'package:elegant_flutter/picker/record/wechat_record_test_page.dart';
 import 'package:flutter/material.dart';
 
 /// 相机有关的
